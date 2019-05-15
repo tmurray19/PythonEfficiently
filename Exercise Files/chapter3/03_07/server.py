b4af07fc2fe78690c0e701891ef4e0cffbc527e3
tmurray19 <t.murray19@nuigalway.ie>
2019-05-15 01:24:15 +0100

    Chapter 3 complete.

---
+++ Exercise Files/chapter3/03_07/server.py
@@ -0,0 +1,35 @@
+
+import flask, jinja2, io, base64
+import stereo
+
+app = flask.Flask(__name__)
+
+template = jinja2.Template("""
+<html>
+    <body>
+        <img src="data:image/png;base64, {{imgdata}}"/>
+    </body>
+</html>    
+""")
+
+def makeimagedata(img):
+    buffer = io.BytesIO()
+    img.save(buffer, format='PNG')
+    return base64.b64encode(buffer.getvalue()).decode('ascii')
+
+
+@app.route('/<sol>')
+def getsol(sol):
+    left, right = stereo.getday(sol).__next__()
+    img = stereo.blend(stereo.getimage(left), stereo.getimage(right))
+    
+    html= template.render(imgdata=makeimagedata(img))
+    
+    return flask.Response(html)
+
+
+@app.route('/')
+def hello_world():
+    return flask.Response("<html><body<p>Hello, World!</p></body></html>")
+
+app.run(host='0.0.0.0')
